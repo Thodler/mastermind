3a7181009f02e8dc1442b200f8f1f8dadec2d24b
Thodler <neoskywalker01@gmail.com>
2022-07-04 20:04:40 +0200

    [UPD] Background color random of Color component

--- src/app/color/color.component.ts
+++ src/app/color/color.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Color} from "../../_models/Color";
 
 @Component({
   selector: 'app-color',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorComponent implements OnInit {
 
+  randomColor!: number
+  colors = [
+    'yellow',
+    'pink',
+    'green',
+    'red',
+    'skyblue',
+    'gold',
+    'orange'
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.randomColor = Math.floor(Math.random() * this.colors.length)
   }
 
 }
