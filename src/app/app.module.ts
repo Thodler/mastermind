16f5d194df92e000bc338cbc9434cfa64aa7506d
Thodler <neoskywalker01@gmail.com>
2022-07-04 18:13:21 +0200

    [UPD] Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ColorComponent } from './color/color.component';
+import { RowComponent } from './row/row.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ColorComponent
+    ColorComponent,
+    RowComponent
   ],
   imports: [
     BrowserModule
